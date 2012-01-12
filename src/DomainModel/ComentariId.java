c5a118789bee1061cc0ad28563eda3568bd4ed31
Clara <clarabayarri@gmail.com>
2012-01-12 22:33:41 +0100

    equals i hash code per les classes ID

--- src/DomainModel/ComentariId.java
+++ src/DomainModel/ComentariId.java
@@ -91,6 +91,30 @@ public class ComentariId implements Serializable {
     }
     
     
-    
-    
+    /**
+     * Funcions necessaries per Hibernate
+     * @param other
+     * @return 
+     */
+    @Override
+    public boolean equals(Object other) {
+         if ( (this == other ) ) return true;
+		 if ( (other == null ) ) return false;
+		 if ( !(other instanceof ComentariId) ) return false;
+		 ComentariId castOther = ( ComentariId ) other; 
+         
+		 return ( (this.getNomHotel()==castOther.getNomHotel()) || ( this.getNomHotel()!=null && castOther.getNomHotel()!=null && this.getNomHotel().equals(castOther.getNomHotel()) ) )
+ && ( (this.getDniClient()==castOther.getDniClient()) || ( this.getDniClient()!=null && castOther.getDniClient()!=null && this.getDniClient().equals(castOther.getDniClient()) ) )
+ && ( (this.getData()==castOther.getData()) || ( this.getData()!=null && castOther.getData()!=null && this.getData().equals(castOther.getData()) ) );
+   }
+   
+    @Override
+   public int hashCode() {
+         int result = 17;
+         
+         result = 37 * result + ( getNomHotel() == null ? 0 : this.getNomHotel().hashCode() );
+         result = 37 * result + ( getDniClient() == null ? 0 : this.getDniClient().hashCode() );
+         result = 37 * result + ( getData() == null ? 0 : this.getData().hashCode() );
+         return result;
+   }
 }
--- src/DomainModel/HabitacioId.java
+++ src/DomainModel/HabitacioId.java
@@ -40,4 +40,28 @@ public class HabitacioId implements Serializable {
         this.numero = numero;
     }
         
+    /**
+     * Operacions necessaries per Hibernate
+     * @param other
+     * @return 
+     */
+    @Override
+    public boolean equals(Object other) {
+         if ( (this == other ) ) return true;
+		 if ( (other == null ) ) return false;
+		 if ( !(other instanceof HabitacioId) ) return false;
+		 HabitacioId castOther = ( HabitacioId ) other; 
+         
+		 return ( (this.getNomHotel()==castOther.getNomHotel()) || ( this.getNomHotel()!=null && castOther.getNomHotel()!=null && this.getNomHotel().equals(castOther.getNomHotel()) ) )
+ && (this.getNumero()==castOther.getNumero());
+   }
+   
+    @Override
+   public int hashCode() {
+         int result = 17;
+         
+         result = 37 * result + ( getNomHotel() == null ? 0 : this.getNomHotel().hashCode() );
+         result = 37 * result + this.getNumero();
+         return result;
+   }
 }
