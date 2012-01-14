83a7dd3392319464d553bfef9c3de13024024f6a
Clara <clarabayarri@gmail.com>
2012-01-14 17:01:50 +0100

    comentari test arreglat

--- test/DomainModel/ComentariTest.java
+++ test/DomainModel/ComentariTest.java
@@ -28,25 +28,24 @@ public class ComentariTest {
     static String descripcio = "comentari de prova";
     static Date d = new Date();
   
-    
     public ComentariTest() {
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception { 
-        //ComentariId id = new ComentariId(nomH, dniC,d);
         try {
             session = HibernateUtil.getSessionFactory().getCurrentSession();
             session.beginTransaction();
+            
             Poblacio poblacio = new Poblacio(nomPoblacio);
-            session.saveOrUpdate(poblacio);
+            session.persist(poblacio);
             Hotel hotel = new Hotel(nomH,"descripcio",nomPoblacio,null);
-            session.saveOrUpdate(hotel);
+            session.persist(hotel);
             Client client = new Client(dniC,"nom prova","cognom","email");
-            session.saveOrUpdate(client);
-            ComentariId id = new ComentariId(nomH, dniC,d);
-            Comentari com = new Comentari(id, 7, descripcio);
-            session.saveOrUpdate(com);
+            session.persist(client);
+            Comentari com = new Comentari(new ComentariId(nomH, dniC,d), 7, descripcio);
+            session.persist(com);
+            
         } catch (RuntimeException e) {
             session.getTransaction().rollback();
             e.printStackTrace();
@@ -58,6 +57,8 @@ public class ComentariTest {
         if (session != null) {
             Comentari com =(Comentari) session.get(Comentari.class,new ComentariId(nomH,dniC,d));
             session.delete(com);
+            Client client = (Client) session.get(Client.class, dniC);
+            session.delete(client);
             Hotel hotel = (Hotel) session.get(Hotel.class, nomH);
             session.delete(hotel);
             Poblacio poblacio = (Poblacio)session.get(Poblacio.class, nomPoblacio);
