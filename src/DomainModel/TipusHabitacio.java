155688ff3f221f2f367cae9658de3ad8af75bf37
Lluís Gómez Hernando <lluisgh28@hotmail.com>
2012-01-15 15:51:19 +0100

    TipusHabitacio: javadoc

--- src/DomainModel/TipusHabitacio.java
+++ src/DomainModel/TipusHabitacio.java
@@ -15,13 +15,26 @@ import java.util.Set;
 public class TipusHabitacio {
     String nom;
     Integer capacitat;
-    String descripcio;
+    String descripcio;    
+    /**
+     * Pels canvis en el disseny del cas d'ús ReservarHabitació,
+     * TipusHabitació té navegabilitat cap a les seves habitacions.
+     */
     Set<Habitacio> habitacions;
     
-    public TipusHabitacio() {
-        
+    /**
+     * Creadora per defecte. Requerida per Hibernate.
+     */
+    public TipusHabitacio() {      
     }
     
+    /**
+     * Creadora amb els paràmetres necessaris per a inicialitzar tots els
+     * atributs.
+     * @param nom
+     * @param capacitat
+     * @param descripcio 
+     */
     public TipusHabitacio(String nom, Integer capacitat, String descripcio) {
         this.nom = nom;
         this.capacitat = capacitat;
@@ -29,9 +42,18 @@ public class TipusHabitacio {
         this.habitacions = new HashSet<Habitacio>();
     }
     
-    
+    /**
+     * Retorna el número d'habitacions disponibles d'aquests tipus a l'hotel
+     * nomHotel, entre les dates indicades i per al nombre d'ocupants donat.
+     * Aquest mètode no apareixia al disseny del primer lliurament. És necessari
+     * degut als canvis en el disseny del cas d'ús ReservarHabitació.
+     * @param dIni
+     * @param dFi
+     * @param nomHotel
+     * @param numOcup
+     * @return
+     */
     public Integer numDisp(Date dIni, Date dFi, String nomHotel, Integer numOcup) {
-
         int num = 0;
         if (this.capacitat >= numOcup) {
             for (Habitacio hab : habitacions) {
@@ -41,43 +63,86 @@ public class TipusHabitacio {
         return num;
     }
     
+    /**
+     * Retorna el numero d'una habitacio del tipus que és de l'hotel nomHotel
+     * i està disponible entre les dates dIni i dFi.
+     * Aquest mètode no apareixia al disseny del primer lliurament. És necessari
+     * degut als canvis en el disseny del cas d'ús ReservarHabitació.
+     * @param nomHotel
+     * @param dIni
+     * @param dFi
+     * @return 
+     */
     public Integer obteNumeroHabLliure(String nomHotel, Date dIni, Date dFi) {
         for (Habitacio h : habitacions) {
             if (h.esDelHotel(nomHotel) && h.estaDisp(dIni, dFi)) 
                 return h.getNumero();
         }
-        
         return null;
     }
 
+    /**
+     * Getter de la capacitat. Requerit per Hibernate.
+     * Degut als canvis en el disseny del cas d'ús ReservarHabitació, aquesta
+     * operació només és necessària per al Hibernate.
+     * @return 
+     */
     public Integer getCapacitat() {
         return capacitat;
     }
 
+    /**
+     * Setter de la capacitat. Requerit per Hibernate. 
+     * @param capacitat 
+     */
     public void setCapacitat(Integer capacitat) {
         this.capacitat = capacitat;
     }
 
+    /**
+     * Getter de la descripció. Requerit per Hibernate.
+     * @return 
+     */
     public String getDescripcio() {
         return descripcio;
     }
 
+    /**
+     * Setter de la descripció. Requerit per Hibernate.
+     * @param descripcio 
+     */
     public void setDescripcio(String descripcio) {
         this.descripcio = descripcio;
     }
 
+    /**
+     * Getter de les habitacions associades. Requerit per Hibernate.
+     * @return 
+     */
     public Set<Habitacio> getHabitacions() {
         return habitacions;
     }
 
+    /**
+     * Setter de les habitacions associades. Requerit per Hibernate.
+     * @param habitacions 
+     */
     public void setHabitacions(Set<Habitacio> habitacions) {
         this.habitacions = habitacions;
     }
 
+    /**
+     * Getter del nom.
+     * @return 
+     */
     public String getNom() {
         return nom;
     }
 
+    /**
+     * Setter del nom. Requerit per Hibernate.
+     * @param nom
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
